0e70692585597fbedd9cfb18be857c23e253f8cc
Adriano <adrianosenny@gmail.com>
2023-12-05 11:41:16 -0300

    Feature: Add bg-color when scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -75,3 +75,12 @@ searchButtonClose.addEventListener('click', () => {
     header.classList.add('top-0');
     header.classList.remove('top-[72px]');
 });
+
+// ADICIONANDO CLASS DEPOIS DE UM SCROLL
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 100) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+});
