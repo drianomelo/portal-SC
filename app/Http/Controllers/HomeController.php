5bbec730a10a46323b34e7029c504457a0e2f3a9
Adriano <adrianosenny@gmail.com>
2023-12-05 13:14:08 -0300

    Feature: Create Home and Api Controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class HomeController extends Controller
+{
+    protected $apiController;
+    public function __construct(ApiController $apiController)
+    {
+        $this->apiController = $apiController;
+    }
+    public function index(){
+
+        // $channelId = 'UCRxSvC9upW2HOAg9zmf2vrg';
+        // $videos = $this->apiController->apiYoutubeGet($channelId);
+
+        // return view('layouts.main', compact('videos'));
+        return view('layouts.main');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.main');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// API YOUTUBE
+Route::get('/latest-videos/{channelId}', [ApiController::class, 'apiYoutubeGet']);
