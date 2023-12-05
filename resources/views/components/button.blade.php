fdf84b66945a12ac6e1bb7b2f3e11c11575ec13c
Adriano <adrianosenny@gmail.com>
2023-12-05 11:42:59 -0300

    Feature: Create component button

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $href }}" class="flex items-center gap-6 px-8 py-4 text-sm font-extrabold uppercase border border-gray-300 rounded-full">
+    {{ $name }}
+    <i class="fa-solid fa-{{$icon}}"></i>
+</a>
