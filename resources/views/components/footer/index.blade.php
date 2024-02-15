157c4eed22cd0676cdee1a7b58ef1091f72153fa
Adriano <adrianosenny@gmail.com>
2024-02-15 14:14:32 -0300

    Feature: Criando componente footer

---
+++ resources/views/components/footer/index.blade.php
@@ -0,0 +1,59 @@
+<footer class="relative">
+    <span class="absolute z-40 bg-blue-900 rounded-full bottom-7 -left-20 w-52 h-52"></span>
+    <span class="absolute z-30 bg-yellow-400 rounded-full bottom-3 -left-0 w-60 h-60"></span>
+    <span class="absolute right-0 z-30 bg-blue-900 rounded-full bottom-3 w-60 h-60"></span>
+    <span class="absolute z-40 bg-yellow-400 rounded-full bottom-8 right-28 w-52 h-52"></span>
+
+    <div class="relative bottom-0 z-50 w-full border-t-[6px] border-blue-900 bg-blue-950">
+        <div class="max-w-[1200px] mx-auto py-12 flex justify-between">
+            <a href="/">
+                <img class="h-[100px]" src="https://transparencia.saocristovao.se.gov.br/imagens/logo_rodape.png"
+                    alt="">
+            </a>
+
+            <div class="flex flex-col gap-2">
+                <h4 class="font-semibold text-white ">Endereço</h4>
+                <span class="w-5 h-[2px] bg-white bg-opacity-50"></span>
+                <div class="flex flex-col gap-1 text-xs text-white text-opacity-50">
+                    <span>Paço Municipal, S/N, </span>
+                    <span>Praça São Francisco,</span>
+                    <span>Centro Histórico,</span>
+                    <span>49100-071</span>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <h4 class="font-semibold text-white ">Horários e Contatos</h4>
+                <span class="w-5 h-[2px] bg-white bg-opacity-50"></span>
+                <div class="flex flex-col gap-1 text-xs text-white text-opacity-50">
+                    <span>Segunda a Sexta - 8h às 14h</span>
+                    <span>Sábado e Domingo - Fechado</span>
+                    <span>Fixo - (79) 3045-4910</span>
+                </div>
+            </div>
+
+            <div class="flex flex-col gap-2">
+                <div class="flex flex-col items-start gap-1 text-xs text-white">
+                    <div class="flex gap-2">
+                        <a class="flex items-center justify-center w-8 h-8 border border-white rounded-full cursor-pointer border-opacity-20"
+                            href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a class="flex items-center justify-center w-8 h-8 border border-white rounded-full cursor-pointer border-opacity-20"
+                            href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a class="flex items-center justify-center w-8 h-8 border border-white rounded-full cursor-pointer border-opacity-20"
+                            href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a class="flex items-center justify-center w-8 h-8 border border-white rounded-full cursor-pointer border-opacity-20"
+                            href="#"><i class="fa-brands fa-youtube"></i></a>
+                    </div>
+                    <button
+                        class="flex items-center w-full justify-between px-4 py-2.5 my-2 text-xs font-extrabold uppercase border border-gray-300 rounded-full">
+                        Mapa do Site
+                        <i class="fa-solid fa-map-location-dot"></i>
+                    </button>
+                </div>
+            </div>
+
+            <a href="/">
+                <img class="h-[100px]" src="https://www.saocristovao.se.gov.br/imagens/radar_svg.svg" alt="">
+            </a>
+        </div>
+    </div>
+</footer>
