a35d9d92999aab39ed230a761dd7a4fb333a3d3c
Adriano <adrianosenny@gmail.com>
2024-02-16 16:07:21 -0300

    Feature: Criando view de municipio - viceprefeito

---
+++ resources/views/municipio/viceprefeito.blade.php
@@ -0,0 +1,60 @@
+<x-layout.index name="viceprefeito">
+
+    <!-- SECTION HOME -->
+    <section
+        class="w-[110%] h-[30vh] homee rounded-b-[35%] relative flex justify-center items-center
+            before:content-[''] before:block before:rounded-b-[35%] before:absolute before:w-full before:h-full before:bg-blue-950 before:opacity-[.85]">
+    </section>
+
+    <!-- SECTION PROFILE -->
+    <section class="max-w-[1200px] mx-auto relative z-50 my-10">
+        <div class="flex items-center justify-between mb-6 -mt-28">
+            <div class="flex gap-3">
+                <div
+                    class="w-40 h-40 rounded-full border-4 border-white bg-[url(https://www.saocristovao.se.gov.br/imagens/orgaos/GABVP.JPG)] bg-cover">
+                </div>
+                <div class="flex flex-col text-white mt-7">
+                    <span class="font-semibold">Paulo Roberto de Santana Júnior</span>
+                    <span class="text-xs font-light">Vice-Prefeito - São Cristóvão</span>
+                    <div class="mt-2.5 flex gap-2">
+                        <a class="relative flex items-center justify-center w-8 h-8 rounded-full cursor-pointer bg-blue-950 border-opacity-20"
+                            href="#">
+                            <span
+                                class="absolute w-4 h-4 bg-yellow-400 rounded-full -bottom-[1px] -right-[2px] -z-10"></span>
+                            <i class="text-white fa-brands fa-instagram"></i>
+                        </a>
+                        <a class="relative flex items-center justify-center w-8 h-8 rounded-full cursor-pointer bg-blue-950 border-opacity-20"
+                            href="#">
+                            <span
+                                class="absolute w-4 h-4 bg-yellow-400 rounded-full -bottom-[1px] -right-[2px] -z-10"></span>
+                            <i class="text-white fa-brands fa-x-twitter"></i></a>
+                        <a class="relative flex items-center justify-center w-8 h-8 rounded-full cursor-pointer bg-blue-950 border-opacity-20"
+                            href="#">
+                            <span
+                                class="absolute w-4 h-4 bg-yellow-400 rounded-full -bottom-[1px] -right-[2px] -z-10"></span>
+
+                            <i class="text-white fa-brands fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <button
+                class="px-4 py-2 mb-10 text-sm font-semibold text-white bg-yellow-400 rounded-md shadow-md">Contate-me</button>
+        </div>
+
+        <div>
+            <div class="min-w-[1200px] mx-auto gap-4 flex flex-col justify-between ">
+                <x-title img="" h2="Sobre" p="Conheça um pouco sobre nosso Vice-Prefeito"></x-title>
+                <p
+                    class="first-letter:text-5xl first-letter:font-bold first-letter:mr-3 first-letter:text-yellow-400 first-letter:float-left">
+                    Paulo Roberto de Santana Júnior, Paulo Júnior, exerceu por dois mandatos o cargo de vereador,
+                    ocupando, ainda, a Presidência da Câmara. Filho do ex-Vereador Paulinho dos Correios e de dona
+                    Edelzita Cardoso, é sancristovense, servidor público do Estado,e ocupou por quatro meses a
+                    Secretaria do Meio Ambiente em 2011. Bacharel em Direito, é também pós-graduando em Gestão e Direito
+                    Municipal.</p>
+            </div>
+
+
+        </div>
+    </section>
+
+</x-layout.index>
