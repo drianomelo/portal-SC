2229e13307cc1851262b08aeef68405511927e69
Adriano Melo <driano@academico.ufs.br>
2024-02-18 13:52:40 -0300

    Feature: Criando view post

---
+++ resources/views/publicacao/post.blade.php
@@ -0,0 +1,224 @@
+<x-layout.index name="post">
+    <!-- SECTION HOME -->
+    <section
+        class="w-[110%] h-[145px] homee rounded-b-[35%] relative flex justify-center items-end
+         before:content-[''] before:block before:rounded-b-[35%] before:absolute before:w-full before:h-full before:bg-blue-950 before:opacity-[.85]">
+
+        <div class="min-w-[1200px] mx-auto relative -mb-3">
+            <x-caminho :caminhos="[['nome' => 'Início', 'url' => '/'], ['nome' => 'Publicação', 'url' => '/publicacao']]" :last="['nome' => 'Infraestrutura', 'url' => '/publicacao/infraestutura']" />
+        </div>
+    </section>
+
+    {{-- <div class="max-w-[1200px] mx-auto mt-10 mb-5 overflow-hidden">
+        <div class="w-full swiperCategory">
+
+            <div class="swiper-wrapper">
+                <x-swiper-category category="Infraestrutura"
+                    img="https://publicacao.saocristovao.se.gov.br/storage/post/prefeitura-de-sao-cristovao-investe-mais-de-r-500-mil-em-obras-no-bairro-irineu-neri-2023-11-24-6560ba209df6e.JPG" />
+                <x-swiper-category category="Gabinete"
+                    img="http://publicacao.saocristovao.se.gov.br/storage/post/prefeito-marcos-santana-destaca-avancos-de-sua-gestao-na-abertura-dos-trabalhos-legislativos-na-camara-de-sao-cristovao-2024-02-16-65cf92339fe67.JPG" />
+                <x-swiper-category category="Saúde"
+                    img="http://publicacao.saocristovao.se.gov.br/storage/post/ubs-sinval-jose-de-oliveira-realiza-mutirao-de-atendimento-para-hipertensos-e-diabeticos-em-sao-cristovao-2024-02-10-65c7799d28072.jpeg" />
+                <x-swiper-category category="Fumctur"
+                    img="https://publicacao.saocristovao.se.gov.br/storage/post/carnaval-dos-carnavais-reune-alegria-e-tradicao-no-centro-historico-de-sao-cristovao-2024-02-13-65cbf7e5d235f.jpeg" />
+                <x-swiper-category category="Infraestrutura"
+                    img="https://publicacao.saocristovao.se.gov.br/storage/post/prefeitura-de-sao-cristovao-investe-mais-de-r-500-mil-em-obras-no-bairro-irineu-neri-2023-11-24-6560ba209df6e.JPG" />
+                <x-swiper-category category="Gabinete"
+                    img="http://publicacao.saocristovao.se.gov.br/storage/post/prefeito-marcos-santana-destaca-avancos-de-sua-gestao-na-abertura-dos-trabalhos-legislativos-na-camara-de-sao-cristovao-2024-02-16-65cf92339fe67.JPG" />
+                <x-swiper-category category="Saúde"
+                    img="http://publicacao.saocristovao.se.gov.br/storage/post/ubs-sinval-jose-de-oliveira-realiza-mutirao-de-atendimento-para-hipertensos-e-diabeticos-em-sao-cristovao-2024-02-10-65c7799d28072.jpeg" />
+                <x-swiper-category category="Fumctur"
+                    img="https://publicacao.saocristovao.se.gov.br/storage/post/carnaval-dos-carnavais-reune-alegria-e-tradicao-no-centro-historico-de-sao-cristovao-2024-02-13-65cbf7e5d235f.jpeg" />
+            </div>
+
+        </div>
+    </div> --}}
+
+    <section class="max-w-[1200px] mt-10 mb-10 flex items-start gap-12">
+        <div class="flex flex-col w-full">
+            <div class="relative ">
+                <div
+                    class="absolute z-50 w-fit right-0 top-3 px-3 text-center rounded-s h-[5%] flex items-center bg-blue-950 bg-opacity-70">
+                    <span class="flex items-center gap-1.5 mt-1 text-[11px] font-bold text-yellow-400">
+                        <i class="text-xs text-yellow-400 fa-solid fa-calendar-days"></i>
+                        04/02/2024
+                    </span>
+                </div>
+
+                <div
+                    class="absolute z-50 bottom-0 px-3 text-center rounded-b-md h-[100%] flex items-end gradient-post-bottom">
+                    <h2 class="mb-6 text-xl font-semibold text-white uppercase">Com quase 75% de execução, obra de
+                        construção
+                        da
+                        nova sede da
+                        Ditin chega à parte final</h2>
+                </div>
+                <div class="absolute bottom-0 rounded-b-md w-full h-[40%]  gradient-post-bottom">
+                </div>
+
+                <img class="w-full rounded-md shadow-md "
+                    src="https://publicacao.saocristovao.se.gov.br/storage/post/com-quase-75-de-execucao-obra-de-construcao-da-nova-sede-da-ditin-chega-a-parte-final-2024-02-04-65bff6bd649f3.JPG"
+                    alt="">
+            </div>
+
+            <div class="px-3 mt-5">
+                <p class="mb-5">
+                    A Diretoria de Tecnologia da Informação da Prefeitura de São Cristóvão (Ditin) vai contar com uma
+                    nova
+                    sede em breve. A construção do prédio que vai abrigar o órgão já se aproxima do percentual de
+                    conclusão
+                    de 75% e a previsão é de que o local seja inaugurado até março deste ano.
+                </p>
+
+                <p class="mb-5">
+                    Com a nova sede da Ditin, a administração municipal vai garantir gestão e segurança da tecnologia de
+                    informação, promovendo avanços na implantação de sistemas de controle e proteção de dados públicos.
+                </p>
+
+                <p class="mb-5">
+                    Para o secretário de infraestrutura, Júlio Nascimento Júnior, a obra reforça o compromisso da gestão
+                    municipal com uma administração eficiente e inovadora:
+                </p>
+
+                <p class="mb-5">
+                    “Vamos construir um prédio completamente novo e uma diretoria que vai cuidar do nosso futuro, com
+                    uma
+                    gestão mais inovadora, tecnológica e capaz de melhorar a qualidade dos nossos serviços”.
+                </p>
+
+                <div class="relative mt-10 mb-5">
+                    <div
+                        class="absolute z-50 w-fit right-0 bottom-3 px-3 text-center rounded-s h-[5%] flex items-center bg-blue-950 bg-opacity-70">
+                        <span class="flex items-center gap-1.5 text-[11px] font-bold text-yellow-400">
+                            Júlio Nascimento Júnior, secretário de infraestrutura
+                        </span>
+                    </div>
+
+                    <img class="rounded-md shadow-md"
+                        src="https://publicacao.saocristovao.se.gov.br/laravel-filemanager/photos/17/23-04-10%2014-08-05%209583.jpg"
+                        alt="">
+                </div>
+
+                <p class="mb-5">
+                    A engenheira fiscal da Seminfra, Ana Paula Marques de Andrade explica que o imóvel contará com dois
+                    pavimentos, sendo o térreo composto de recepção, arquivo, duas salas técnicas, banheiro acessível e
+                    copa para funcionários. Já o pavimento superior contemplará três salas técnicas, depósito de
+                    material de limpeza e banheiro acessível. Ela também destaca a preocupação da gestão em garantir uma
+                    estrutura com acessibilidade para facilitar o acesso de todos:
+                </p>
+
+                <p class="mb-5">
+                    “O prédio está projetado em atendimento às normas de acessibilidade, no que diz respeito ao passeio
+                    público, escada interna e externa, plataforma elevatória, rampas de acesso e banheiros acessíveis”.
+                </p>
+
+                <div class="relative mt-10 mb-5">
+                    <div
+                        class="absolute z-50 w-fit right-0 bottom-3 px-3 text-center rounded-s h-[5%] flex items-center bg-blue-950 bg-opacity-70">
+                        <span class="flex items-center gap-1.5 text-[11px] font-bold text-yellow-400">
+                            Ana Paula Marques de Andrade, engenheira fiscal da Seminfra
+                        </span>
+                    </div>
+
+                    <img class="rounded-md shadow-md"
+                        src="https://publicacao.saocristovao.se.gov.br/laravel-filemanager/photos/17/HXSJ1015.JPG"
+                        alt="">
+                </div>
+
+                <div class="relative mt-10 mb-5">
+                    <div
+                        class="absolute z-50 w-fit right-0 bottom-3 px-3 text-center rounded-s h-[5%] flex items-center bg-blue-950 bg-opacity-70">
+                        <span class="flex items-center gap-1.5 text-[11px] font-bold text-yellow-400">
+                            Foto interna do novo prédio da DITIN
+                        </span>
+                    </div>
+
+                    <img class="rounded-md shadow-md"
+                        src="https://publicacao.saocristovao.se.gov.br/laravel-filemanager/photos/17/IMG_5246.JPG"
+                        alt="">
+                </div>
+
+                <p class="mb-5">
+                    Josenilson Silva Santos, diretor de tecnologia da informação da Prefeitura, ressalta que o novo
+                    prédio
+                    vai abrigar o DATACENTER que opera com armazenamento, segurança, confidencialidade e autenticidade
+                    de
+                    todos os dados que são trafegados internamente na rede do município.
+                </p>
+
+                <p class="mb-5">
+                    O diretor também pontua a importância da obra para o ganho de eficiência na execução de serviços
+                    prestados pela gestão, através de uma evolução da estrutura disponível para a tecnologia de
+                    informação.
+                </p>
+
+                <p class="mb-5">
+                    A obra não é apenas uma estrutura física, mas um espaço próprio para que a diretoria possa
+                    desempenhar
+                    suas atividades, além de oferecer aos servidores o conforto e a qualificação, o município vai
+                    promover a
+                    implantação da tecnologia em sentido avançado, com mais qualidade na prestação de serviços para
+                    alcançar
+                    toda sociedade”, concluiu.
+                </p>
+
+                <div class="relative mt-10 mb-5">
+                    <div
+                        class="absolute z-50 w-fit right-0 bottom-3 px-3 text-center rounded-s h-[5%] flex items-center bg-blue-950 bg-opacity-70">
+                        <span class="flex items-center gap-1.5 text-[11px] font-bold text-yellow-400">
+                            Josenilson Silva Santos, diretor de tecnologia da informação da Prefeitura
+                        </span>
+                    </div>
+
+                    <img class="w-full rounded-md shadow-md"
+                        src="https://publicacao.saocristovao.se.gov.br/laravel-filemanager/photos/17/WhatsApp%20Image%202024-02-04%20at%2014.37.06.jpeg"
+                        alt="">
+                </div>
+
+                <div class="relative mt-10 mb-5">
+                    <div
+                        class="absolute z-50 w-fit right-0 bottom-3 px-3 text-center rounded-s h-[5%] flex items-center bg-blue-950 bg-opacity-70">
+                        <span class="flex items-center gap-1.5 text-[11px] font-bold text-yellow-400">
+                            Construção do novo prédio da DITIN
+                        </span>
+                    </div>
+
+                    <img class="w-full rounded-md shadow-md"
+                        src="https://publicacao.saocristovao.se.gov.br/laravel-filemanager/photos/17/IMG_5259.JPG"
+                        alt="">
+                </div>
+
+                <div class="flex items-center gap-2">
+                    <i class="p-2.5 text-base text-gray-400 border rounded-full fa-solid fa-camera-retro"></i>
+                    <div class="flex flex-col">
+                        <span class="text-[10px] italic font-light">Fotos por</span>
+                        <span class="text-sm font-extrabold">Dani Santos</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="max-w-[350px]">
+            <h3 class="relative mb-4 font-bold uppercase w-fit">
+                Mais de Infraestrutura:
+            </h3>
+            <div class="flex flex-col gap-2">
+                <x-card-publicacao-small
+                    src="https://publicacao.saocristovao.se.gov.br/storage/post/prefeitura-de-sao-cristovao-investe-mais-de-r-500-mil-em-obras-no-bairro-irineu-neri-2023-11-24-6560ba209df6e.JPG"
+                    alt="teste" href=""
+                    title="Prefeitura de São Cristóvão investe mais de R$ 500 mil em obras no bairro Irineu Neri"
+                    tag="infraestrutura" data="24/11/2023"
+                    desc="Através da Secretaria Municipal de Infraestrutura (Seminfra), a Prefeitura de São Cristóvão está realizando importante obras que mudam a realidade dos moradores do bairro Irineu Neri. Com a recuperação da pavimentação da rua São Domingos e a obra de drenagem e pavimentação asfáltica da Av. Irineu Neri, a Prefeitura investe mais de 500 mil reais, oriundos de recursos próprios do município, em ações que valorizam o cidadão e melhoram a mobilidade urbana da cidade."
+                    fotografo="Dani Santos" />
+                <x-card-publicacao-small
+                    src="http://publicacao.saocristovao.se.gov.br/storage/post/equipe-da-seminfra-apresenta-projetos-de-requalificacao-de-pracas-e-revitalizacao-de-atracadouro-2024-02-07-65c3c0239e501.JPG"
+                    alt="teste" href=""
+                    title="Equipe da Seminfra apresenta projetos de requalificação de praças e revitalização de atracadouro "
+                    tag="infraestrutura" data="07/02/2024"
+                    desc='Durante reunião entre o prefeito Marcos Santana e o secretário Municipal de Infraestrutura (Seminfra), Júlio Júnior, foram apresentados projetos que visam revitalizar o atracadouro do "Catamarã", no Centro Histórico e promover a requalificação de duas praças na cidade: a Praça da Alameda no Eduardo Gomes e a Praça Horácio Souza Lima, localizada no bairro Rosa Elze, em São Cristóvão.'
+                    fotografo="Heitor Xavier" />
+
+            </div>
+        </div>
+    </section>
+</x-layout.index>
