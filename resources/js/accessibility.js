6586daddaa92b60da27298eca903894a08807a29
Adriano <adrianosenny@gmail.com>
2024-02-21 14:26:07 -0300

    Feature: Criando função para aumentar todas as fontes da pagina (increaseFont())

--- resources/js/accessibility.js
+++ resources/js/accessibility.js
@@ -1,6 +1,6 @@
-const accessibilityButton = document.querySelector("#btn-accessibility");
-const accessibilityMenu = document.querySelector("#menu-accessibility");
-const accessibilityCloseButton = document.querySelector("#menu-accessibility");
+const accessibilityButton = document.getElementById("btn-accessibility");
+const accessibilityMenu = document.getElementById("menu-accessibility");
+const accessibilityCloseButton = document.getElementById("close-accessibility");
 
 accessibilityButton.addEventListener("click", () => {
     accessibilityMenu.classList.remove("bottom-10");
@@ -17,3 +17,44 @@ accessibilityCloseButton.addEventListener("click", () => {
     accessibilityMenu.classList.remove("z-[999]");
     accessibilityMenu.classList.add("-z-[999]");
 });
+
+const tags = document.querySelectorAll(".text");
+const increaseButton = document.getElementById("aumentar");
+const nameIncreaseButton = increaseButton.querySelector(".name");
+let increaseCont = 0;
+let initName = 100;
+let initFontSize = [];
+
+increaseButton.addEventListener("click", () => {
+    increaseFont();
+});
+
+function increaseFont() {
+    if (increaseCont === 0) {
+        tags.forEach((tag, i) => {
+            const style = window.getComputedStyle(tag).fontSize;
+            const fontSize = parseFloat(style);
+            initFontSize.push(fontSize);
+            tag.style.fontSize = initFontSize[i] * 1.25 + "px";
+        });
+        initName = initName + 25;
+        nameIncreaseButton.textContent = `${initName}%`;
+        increaseCont++;
+    } else if (increaseCont === 3) {
+        tags.forEach((tag, i) => {
+            tag.style.fontSize = initFontSize[i] + "px";
+        });
+        initName = 100;
+        nameIncreaseButton.textContent = "100%";
+        increaseCont = 0;
+    } else {
+        tags.forEach((tag) => {
+            const style = window.getComputedStyle(tag).fontSize;
+            const fontSize = parseFloat(style);
+            tag.style.fontSize = fontSize * 1.25 + "px";
+        });
+        initName = initName + 25;
+        nameIncreaseButton.textContent = `${initName}%`;
+        increaseCont++;
+    }
+}
