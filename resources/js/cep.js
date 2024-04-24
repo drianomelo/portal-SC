d3cccfe27509ef7dee6670fbcec7531b859b7888
Adriano <adrianosenny@gmail.com>
2024-04-24 10:12:35 -0300

    Refactor: Alteraçõs na view de cep para receber nova API

--- resources/js/cep.js
+++ resources/js/cep.js
@@ -1,24 +1,23 @@
 const form = document.getElementById("cep-form");
-const inputForm = document.getElementById("input-rua");
+const inputForm = document.getElementById("input-cep");
 const labelForm = document.getElementById("label-rua");
 const ruas = document.getElementById("ruas");
+const btnsCep = document.querySelectorAll(".btn-cep button");
+const resultadoCep = document.querySelector("#resultado-cep");
 
-inputForm.addEventListener("focus", () => {
-    inputFocus();
-});
-
-inputForm.addEventListener("blur", () => {
-    if (inputForm.value == "") {
-        inputBlur();
-    }
+btnsCep.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        handleClickButton(btn.id);
+    });
 });
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    const inputForm = document.getElementById("input-cep");
+
     const street = inputForm.value;
 
-    inputForm.value = "";
     ruas.innerHTML = "";
     getCEP(street).then((data) => {
         data.forEach((rua) => {
@@ -44,8 +43,9 @@ form.addEventListener("submit", (event) => {
                     </button>
                     `;
         });
+        resultadoCep.textContent = `${data.length} resultados para "${street}"`;
     });
-    inputBlur();
+
 });
 
 async function getCEP(street) {
@@ -57,19 +57,6 @@ async function getCEP(street) {
     return convertedConnection;
 }
 
-function inputFocus() {
-    labelForm.classList.remove("bottom-2/4");
-    labelForm.classList.add("bottom-full");
-    labelForm.classList.remove("left-8");
-    labelForm.classList.add("left-12");
-}
-
-function inputBlur() {
-    labelForm.classList.remove("bottom-full");
-    labelForm.classList.add("bottom-2/4");
-    labelForm.classList.remove("left-12");
-    labelForm.classList.add("left-8");
-}
 
 async function getMap(cep) {
     const connection = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
@@ -82,4 +69,46 @@ async function getMap(cep) {
         `http://maps.google.com/maps?z=12&t=m&q=loc:${lat}+${lng}`,
         "_blank"
     );
+}
+
+function handleClickButton(selector) {
+    if (selector === "btn-rua") {
+        form.innerHTML = `
+        <div class="relative flex items-center justify-center mx-auto mb-6 w-fit">
+            <label
+                class="absolute px-3 font-bold text-blue-900 transition-all bg-white dark:bg-zinc-900 dark:text-yellow-500 left-8 bottom-12"
+                for="input-cep" id="label-rua">Nome da Rua</label>
+            <input type="text" required
+                class="w-[400px] px-8 py-4 bg-white border-2 border-blue-900 rounded-full outline-none dark:border-yellow-500 dark:bg-zinc-900 dark:text-white"
+                id="input-cep">
+        </div>
+
+        <input type="submit" value="Pesquisar CEP"
+                class="px-4 py-2.5 font-bold text-yellow-600 bg-yellow-400 border-2 border-yellow-500 rounded-md cursor-pointer w-fit">
+        `;
+    } else if (selector === "btn-bairro") {
+        form.innerHTML = `
+        <select name="input-cep" id="input-cep"
+            class="w-[400px] px-8 py-4 bg-white border-2 font-bold text-blue-900 mb-6 border-blue-900 rounded-full outline-none dark:border-yellow-500 dark:bg-zinc-900 dark:text-yellow-500">
+            <option value="Área Rural de São Cristóvão">Área Rural de São Cristóvão</option>
+            <option value="Centro">Centro</option>
+            <option value="Divineia">Divineia</option>
+            <option value="Eduardo Gomes">Eduardo Gomes</option>
+            <option value="Irineu Neri">Irineu Neri</option>
+            <option value="Lourival Batista">Lourival Batista</option>
+            <option value="Luiz Alves">Luis Alves</option>
+            <option value="Madre Paulina">Madre Paulina</option>
+            <option value="Marcelo Deda">Marcelo Deda</option>
+            <option value="Romualdo Prado">Romualdo Prado</option>
+            <option value="Rosa Elze">Rosa Elze</option>
+            <option value="Rosa Maria">Rosa Maria</option>
+            <option value="São Gonçalo">São Gonçalo</option>
+            <option value="Tijuquinha">Tijuquinha</option>
+            <option value="Varzea Grande">Varzea Grande</option>
+        </select>
+
+        <input type="submit" value="Pesquisar CEP"
+                class="px-4 py-2.5 font-bold text-yellow-600 bg-yellow-400 border-2 border-yellow-500 rounded-md cursor-pointer w-fit">
+        `;
+    }
 }
--- resources/views/cep/index.blade.php
+++ resources/views/cep/index.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center w-full overflow-x-hidden">
         <section
             class="w-[110%] h-[390px] bg-home rounded-b-[35%] relative flex justify-center items-center
-     before:content-[''] before:block before:rounded-b-[35%] before:absolute before:w-full before:h-full before:bg-blue-950 before:opacity-[.30] xl:w-[120%] xl:bg-home-xl lg:w-[130%] lg:bg-home-sm lg:justify-start">
+     before:content-[''] before:block before:rounded-b-[35%] before:absolute before:w-full before:h-full before:bg-blue-950 before:opacity-[.30] xl:w-[120%] xl:bg-home-xl lg:w-[130%] lg:bg-home-sm lg:justify-start sm:h-[290px]">
 
             <div class="min-w-[1200px] relative z-10 mt-16 lg:px-5 lg:min-w-fit lg:pl-[180px] sm:pl-[85px]">
                 <h2 class="mb-6 text-6xl font-extrabold text-white sm:text-4xl">Encontre seu CEP</h2>
@@ -13,23 +13,35 @@
                 </p>
             </div>
         </section>
+
+        <div class="relative flex items-center gap-4 mt-6 btn-cep">
+            <button id="btn-rua"
+                class="px-4 py-2.5 font-bold text-yellow-600 bg-yellow-400 border-2 border-yellow-500 rounded-md cursor-pointer w-fit">Pesquisar
+                por Rua</button>
+            <button id="btn-bairro"
+                class="px-4 py-2.5 font-bold text-yellow-600 bg-yellow-400 border-2 border-yellow-500 rounded-md cursor-pointer w-fit">Pesquisar
+                por Bairro</button>
+        </div>
     </div>
 
-    <div class="max-w-[1200px] mx-auto mt-14 sm:mt-6">
+    <div class="max-w-[1200px] mx-auto mt-6 sm:mt-6">
         <form class="flex flex-col items-center w-full" id="cep-form">
-            <div class="relative flex items-center justify-center mx-auto mb-10 w-fit">
+            <div class="relative flex items-center justify-center mx-auto mb-6 w-fit">
                 <label
-                    class="absolute px-3 font-bold text-blue-900 transition-all bg-white dark:bg-zinc-900 dark:text-yellow-500 left-8 bottom-2/4 translate-y-2/4"
-                    for="input-rua" id="label-rua">Nome da Rua</label>
+                    class="absolute px-3 font-bold text-blue-900 transition-all bg-white dark:bg-zinc-900 dark:text-yellow-500 left-8 bottom-12"
+                    for="input-cep" id="label-rua">Nome da Rua</label>
                 <input type="text" required
-                    class="w-[400px] px-8 py-4 bg-white border-2 border-blue-900 rounded-full outline-none dark:border-yellow-500 dark:bg-zinc-900 dark:text-white"
-                    id="input-rua">
+                    class="w-[400px] px-8 py-4 bg-white border-2 border-blue-900 rounded-full outline-none dark:border-yellow-500  dark:bg-zinc-900 dark:text-white"
+                    id="input-cep">
             </div>
+
             <input type="submit" value="Pesquisar CEP"
                 class="px-4 py-2.5 font-bold text-yellow-600 bg-yellow-400 border-2 border-yellow-500 rounded-md cursor-pointer w-fit">
         </form>
 
-        <div id="ruas" class="flex flex-wrap items-start justify-center w-full gap-4 my-10">
+        <p id="resultado-cep" class="mt-6 text-lg font-bold text-center text-blue-950">20 resultados para "Teste"</p>
+
+        <div id="ruas" class="flex flex-wrap items-start justify-center w-full gap-4 my-6">
             <div class="flex items-center bg-blue-900 gap-2 w-[350px] p-3 pl-5 rounded-full">
                 <img class="w-16 ml-2"
                     src="https://anexos.saocristovao.se.gov.br/storage/files/18/portal/icon/location.png"
